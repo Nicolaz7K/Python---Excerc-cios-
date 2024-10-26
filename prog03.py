af3cb5944bcd56feea373f356b1e3822c0d7e0d8
Nicolaz7K <nico.rafa7k@gmail.com>
2024-10-26 18:24:22 -0300

    Add files via upload
    Exercicios

---
+++ prog03.py
@@ -0,0 +1,65 @@
+#excercício 1
+"""
+if __name__ == "__main__":
+    
+    numero = int(input("Escreva o número: "))
+
+    if numero > 0:
+        print("Seu numero é positivo")
+
+    elif numero < 0:
+        print("Seu numero é negativo")
+        
+    else:
+        print("Seu número é zero")
+"""
+#excercicio 2 ok
+""""
+if __name__ == "__main__":
+    idade = int(input("Escreva sua idade: "))
+
+    if idade >= 18:
+        print("Você é maior de idade")
+    elif idade < 18:
+        print("Você é menor de idade")
+    else:
+        print("Você é um alienigena")
+"""    
+#exercício 3
+"""
+if __name__ == "__main__":
+    numero1 = 5
+    numero2 = 10
+    numero3 = 12
+
+    if numero1 > numero2 and numero1 > numero3:
+        print(f"O maior número é o {numero1}")
+    
+    elif numero2 > numero1 and numero2 > numero3:
+        print(f"O maior número é o {numero2}")
+    else:
+        print(f"O maior número é o {numero3}")
+"""
+#exercício 4
+"""
+if __name__ == "__main__":
+    numero1 = int(input("Escreva o primeiro numero "))
+    numero2 = int(input("Escreva o segundo número "))
+    print(int(numero1 + numero2))
+    print(int(numero1 - numero2))
+    print(int(numero1 * numero2))
+    print(int(numero1 / numero2))
+"""
+
+#exercício 5 
+"""
+if __name__ == "__main__":
+    nome = (input("Escreva seu nome: "))
+    idade = int(input("Escreva sua idade: "))
+    sexo = (input("Escreva seu sexo: "))
+
+    if sexo = str():
+"""
+
+
+
